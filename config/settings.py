fa72c2ee0957abd891534d61a45fa0c404e6c1d2
Bertrand <bmiannay@2a02-8429-b344-7a01-87d0-558b-a34f-1c24.rev.sfr.net>
2026-03-10 21:48:00 +0100

    add trusted origins

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,10 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    f'https://{host}' for host in ALLOWED_HOSTS if host not in ('localhost', '127.0.0.1')
+] or ['http://localhost:8000']
+
 
 # Application definition
 
