de3b24f9fa26bb1d4aa625d66ede94e6fb802fa6
BertrandMiannay <bertrandmiannay@gmail.com>
2026-03-20 22:31:21 +0100

    add form creation : WIP

--- helloAssoImporter/urls.py
+++ helloAssoImporter/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.EventFormListView.as_view(), name="inscriptions"),
     path("refresh/", views.refresh_event_forms, name="inscriptions-refresh"),
+    path("create/", views.create_event_form, name="event-form-create"),
     path("<str:form_slug>/", views.EventFormDetailView.as_view(), name="event-form-detail"),
     path("forms/", views.MemberShipFormListView.as_view()),
     path("orders/", views.MemberShipFormOrderListView.as_view()),
